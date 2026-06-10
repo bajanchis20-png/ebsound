3642033c1c714fc97f186f655942b7f8fe02e767
bajanchis20-png <Bajanchis20@gmail.com>
2026-06-10 11:53:15 -0400

    88

--- src/lib/components/Barra-de-music.svelte
+++ src/lib/components/Barra-de-music.svelte
@@ -3,5 +3,5 @@ import {obtenercancion} from '$lib/state/cancion-actual.svelte'
     let cancionActual = $derived(obtenercancion())
 </script>
 <div>
-    <audio src={cancionActual?.audio} controls></audio>
+    <audio src={cancionActual?.audio} controls autoplay></audio>
 </div> 
